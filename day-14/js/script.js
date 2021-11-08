318344d1b434f5139234888dadb311f7f361d822
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-08 17:26:59 +0600

    Js

--- day-14/js/script.js
+++ day-14/js/script.js
@@ -1,64 +1,64 @@
-    var submit = document.getElementById('submit');
-    submit.onclick= function(){
-        var startingNumber = Number(document.getElementById('firstNumber').value);
-        var endingNumber = Number(document.getElementById('endingNumber').value);
+//     var submit = document.getElementById('submit');
+//     submit.onclick= function(){
+//         var startingNumber = Number(document.getElementById('firstNumber').value);
+//         var endingNumber = Number(document.getElementById('endingNumber').value);
         
-          var i;
-        var result = '';
+//           var i;
+//         var result = '';
 
-        var choice = document.getElementsByName('choice');
-        var temp= '';
-        if(choice[0].checked == true){
-            temp ='odd';
-        }else{
-            temp = 'even';
-        }
+//         var choice = document.getElementsByName('choice');
+//         var temp= '';
+//         if(choice[0].checked == true){
+//             temp ='odd';
+//         }else{
+//             temp = 'even';
+//         }
 
-        console.log(temp);
+//         console.log(temp);
 
 
 
-         if(startingNumber<=endingNumber){
+//          if(startingNumber<=endingNumber){
 
-            if(temp== 'even'){
-                for(i = startingNumber; i<=endingNumber; i++){
-                    if(i/2 != 0){
-                        result = result + i +' '; 
-                    }
+//             if(temp== 'even'){
+//                 for(i = startingNumber; i<=endingNumber; i++){
+//                     if(i/2 != 0){
+//                         result = result + i +' '; 
+//                     }
                       
                      
-                }
-            }else{
+//                 }
+//             }else{
 
-                for(i = startingNumber; i<=endingNumber; i++){
-                    if(i%2 == 0){
-                        result = result + i +' '; 
-                    }
-                }
-            }
+//                 for(i = startingNumber; i<=endingNumber; i++){
+//                     if(i%2 == 0){
+//                         result = result + i +' '; 
+//                     }
+//                 }
+//             }
             
-            document.getElementById('result').innerHTML= result;
-}
+//             document.getElementById('result').innerHTML= result;
+// }
          
-        else{
-            if(temp == 'odd'){
+//         else{
+//             if(temp == 'odd'){
                 
-                for(i = startingNumber; i>=endingNumber; i--){
-                    if(i%2 != 0){
-                        result = result + i +' ';
-                    }
+//                 for(i = startingNumber; i>=endingNumber; i--){
+//                     if(i%2 != 0){
+//                         result = result + i +' ';
+//                     }
                     
-            }
-        }else{
+//             }
+//         }else{
 
-                for(i = startingNumber; i>=endingNumber; i--)
-                    if(i%2 == 0){
-                        result = result + i +' ';
-                    }
+//                 for(i = startingNumber; i>=endingNumber; i--)
+//                     if(i%2 == 0){
+//                         result = result + i +' ';
+//                     }
                     
-            }document.getElementById('result').innerHTML= result;
-        }
-}
+//             }document.getElementById('result').innerHTML= result;
+//         }
+// }
 
 
 
@@ -70,6 +70,54 @@
 
 // Index2 Start
 
+var homeBtn = document.getElementById('homeBtn');
+var aboutBtn = document.getElementById('aboutBtn');
+var contactBtn = document.getElementById('contactBtn');
+
+
+var home = document.getElementById('home');
+var about = document.getElementById('about');
+var contact = document.getElementById('contact');
 
 
 
+
+homeBtn.onclick = function(){
+    event.preventDefault();
+    home.setAttribute('class','content-wrapper');
+    about.setAttribute('class','content-wrapper d-none');
+    contact.setAttribute('class','content-wrapper d-none')
+
+
+
+    homeBtn.setAttribute('class','active');
+    aboutBtn.setAttribute('class','');
+    contactBtn.setAttribute('class','');
+}
+
+
+aboutBtn.onclick = function(){
+   
+    event.preventDefault();
+    home.setAttribute('class','content-wrapper d-none');
+    about.setAttribute('class','content-wrapper');
+    contact.setAttribute('class','content-wrapper d-none')
+
+
+    homeBtn.setAttribute('class','');
+    aboutBtn.setAttribute('class','active');
+    contactBtn.setAttribute('class','');
+}
+
+
+contactBtn.onclick = function(){
+    event.preventDefault();
+    home.setAttribute('class','content-wrapper d-none');
+    about.setAttribute('class','content-wrapper d-none');
+    contact.setAttribute('class','content-wrapper ')
+
+
+    homeBtn.setAttribute('class','');
+    aboutBtn.setAttribute('class','');
+    contactBtn.setAttribute('class','active');
+}
