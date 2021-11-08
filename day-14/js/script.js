a0c7178f5a6ff912b4b23ff99e177141afe1f184
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-08 15:14:37 +0600

    JS_Loop

--- day-14/js/script.js
+++ day-14/js/script.js
@@ -2,11 +2,22 @@
     submit.onclick= function(){
         var startingNumber = Number(document.getElementById('firstNumber').value);
         var endingNumber = Number(document.getElementById('endingNumber').value);
-         var choice = document.getElementsByName('choice');
-        var result = document.getElementById('result').innerHTML;
+        
+         
          
         var i;
         var result = '';
+
+        var choice = document.getElementsByName('choice');
+        var temp= '';
+        if(choice[0].checked == true){
+            temp ='odd';
+        }else{
+            temp = 'even';
+        }
+
+        console.log(temp);
+
 
 
          if(startingNumber<=endingNumber){
@@ -18,6 +29,7 @@
             }
             document.getElementById('result').innerHTML= result;
          }else{
+            
             for(i = startingNumber; i>=endingNumber; i--){
                 result = result + i +' ';
                 
