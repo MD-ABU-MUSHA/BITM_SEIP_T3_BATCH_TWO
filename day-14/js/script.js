1261a552bfc1db16660ce0881db3d93640620d8e
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-08 14:47:11 +0600

    JS-Loop

--- day-14/js/script.js
+++ day-14/js/script.js
@@ -1,17 +1,29 @@
 var submit = document.getElementById('submit');
     submit.onclick= function(){
-        var startingNumber = document.getElementById('firstNumber').value;
-        var endingNumber = document.getElementById('endingNumber').value;
+        var startingNumber = Number(document.getElementById('firstNumber').value);
+        var endingNumber = Number(document.getElementById('endingNumber').value);
         var result = document.getElementById('result').innerHTML;
          
         var i;
-        var result = ' ';
+        var result = '';
+
+
+         if(startingNumber<=endingNumber){
+            for(i = startingNumber; i<=endingNumber; i++){
+                result = result + i +' ';
+                
+                 
+            }
+            document.getElementById('result').innerHTML= result;
+         }else{
+            for(i = startingNumber; i>=endingNumber; i--){
+                result = result + i +' ';
+                
+                 
+            }
+            document.getElementById('result').innerHTML= result;
+         }
         
-        for(i = startingNumber; i<=endingNumber; i++){
-            result = result + i +' ';
-            
-             
-        }
-        document.getElementById('result').innerHTML= result;
+       
 
     }
