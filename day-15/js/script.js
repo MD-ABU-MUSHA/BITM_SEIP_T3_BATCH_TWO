54580025fdfdf6e5e948eaf546086dcff1a8849d
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-09 14:40:12 +0600

    js_prime_not_prime

--- day-15/js/script.js
+++ day-15/js/script.js
@@ -2,17 +2,31 @@ var submit = document.getElementById('Submit');
         submit.onclick = function(){
             
             var number = Number(document.getElementById('Number').value);
-            
+            var result = 'prime';
+            for(var i=2; i<number; i++){
+                if(number%i == 0)
+                {
+                    result = 'Not Prime';
+                    break;
+                }else{
+                    continue;
+                }
+            }
+
+
+            document.getElementById('Result').innerHTML = result;
         
 
 
-            if(number%2 == 0){
-                var result = document.getElementById('Result').innerHTML="Even";
+            // if(number%2 == 0){
+            //     var result = document.getElementById('Result').innerHTML="Even";
     
-            }else{
-                var result = document.getElementById('Result').innerHTML="Odd";
+            // }else{
+            //     var result = document.getElementById('Result').innerHTML="Odd";
+
+            // }
+
+
+
         
-        }
-
-
 }
