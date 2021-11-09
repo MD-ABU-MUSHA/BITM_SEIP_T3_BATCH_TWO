97a1f3319822322e97db6c4c2c4500255be03dcb
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-09 15:28:44 +0600

    Js_date_time

--- day-15/js/script.js
+++ day-15/js/script.js
@@ -29,17 +29,38 @@ var submit = document.getElementById('Submit');
 }
 
 
- var dateTime = new Date();
- var month= ['january','February','March','Aprial','May','Jun','July','August','September','Octobor','November','December'];
- var day = ['Sunday','Monday','thursday','Wednessday','Tuesday','Friday','saterday'];
+ 
 
- var hour = dateTime.getHours();
- var minutes = dateTime.getMinutes();
- var second = dateTime.getSeconds();
- var year = dateTime.getFullYear();
 
-var result = day[dateTime.getDay()]+","+month[dateTime.getMonth()]+","+year;
- var watch = hour +":"+ minutes +":"+ second;
 
- console.log(result);
- console.log(watch);
+
+ /*
+    SetInterval():
+    SetTimeout():
+
+
+    */
+
+
+
+    function test(){
+                    var dateTime = new Date();
+            var month= ['january','February','March','Aprial','May','Jun','July','August','September','Octobor','November','December'];
+            var day = ['Sunday','Monday','thursday','Wednessday','Tuesday','Friday','saterday'];
+
+            var hour = dateTime.getHours();
+            var minutes = dateTime.getMinutes();
+            var second = dateTime.getSeconds();
+            var year = dateTime.getFullYear();
+
+            var result = day[dateTime.getDay()]+", "+month[dateTime.getMonth()]+", "+dateTime.getDate()+", "+year;
+            var watch = hour +":"+ minutes +":"+ second;
+
+            document.getElementById('watch').innerHTML=result+"|"+watch;
+    }
+
+    setInterval(test, 1000);
+
+
+
+    // setTimeout(test,1000);
