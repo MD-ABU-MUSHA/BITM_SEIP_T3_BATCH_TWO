b29d9574ce4299038f4e5285086943e5bac0fcce
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-11 14:08:38 +0600

    jquery_Product_listing

---
+++ day-17/js/script.js
@@ -0,0 +1,79 @@
+
+$('#btn').click(function(){
+    var div ='';
+    div += '<div>';
+    div += '<input type="text"/>';
+    div += '<input type="text"/>';
+    div += '</div>';
+
+    $('#wrapper').append(div);
+
+});
+
+
+
+$('#btnOne').click(function(){
+    var firstName = $('#firstName').val();//Return val when no perameter
+    var lastName = $('#lastName').val();
+
+
+
+    var result = firstName +' '+lastName;
+    $('#fullName').val(result);
+});            
+
+
+
+
+
+
+var products = [
+    {id: 1, name: 'Shirt', Price: 2000},
+    {id: 2, name: 'Saree', price: 4000},
+    {id: 3, name: 'T-shirt', price: 1200},
+    {id: 4, name: 'football', price: 1500},
+];
+
+
+var select = '';
+select += '<select>';
+$.each(products, function(key,value){
+    select += '<option>'+value.name+'</option>';
+})
+
+select += '</select>'
+
+
+$('#td1').append(select);
+
+
+$('#addBtn').click(function(){
+    var tr = '';
+    tr += '<tr>';
+    tr += '<td>Name</td>';
+    tr += '<td>200</td>';
+    tr += '<td>2</td>';
+    tr += '<td>400</td>';
+    tr += '<td><button type="button" class="remove-btn">-</button></td>';
+    tr +=  '</tr>';
+
+
+    $('#res').append(tr);
+});
+
+
+
+$(document).on('click','.remove-btn',function(){
+    $(this).closest('tr').remove();
+});
+
+// anather way
+// $(document).on('click','.remove-btn',function(){
+//     $(.remove-btn).closest('tr').remove();
+// });
+
+
+//Wrong
+// $('.remove-btn').click(function(){
+//     $(this).closest('tr').remove();
+// });
